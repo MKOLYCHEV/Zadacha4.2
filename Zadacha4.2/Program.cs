6e3c9ab3ea0911819f0fff82e61a6c3c0dc9ce53
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-11-20 18:10:55 +0300

    Добавьте файлы проекта.

---
+++ Zadacha4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0, n = 0, num;
+            do
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                    n++;
+                else if (num > 0)
+                    p++;
+            } while (num!=0);
+            if (p>n)
+            {
+                Console.WriteLine("Количество положительных чисел больше, чем отрицательных.");
+            }
+            else if(n>p)
+            {
+                Console.WriteLine("Количество отрицательных чисел больше, чем положительных.");
+            }
+            else if (n==p&&p==0)
+            {
+                Console.WriteLine("Ошибка! Первое введенное число - ноль.");
+            }
+            else Console.WriteLine("Количество отрицательных чисел ровно количеству положительных.");
+            Console.ReadKey();
+        }
+    }
+}
